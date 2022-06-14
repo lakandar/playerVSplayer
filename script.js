e4f86fbe6c1306d4366c0a9242ce8a965b09b045
Abu Hasan <lakandar@gmail.com>
2022-06-15 02:22:39 +0600

    Update script.js -Add feature to all button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let player1Score=0;
 let player2Score=0;
 let winningScore=5;
+let gameOver=false;
+let turnPlayer="p1";
 
 //selector
 let p1sEle = document.querySelector("#p1Score");
@@ -14,32 +16,67 @@ let resetbtnEle = document.querySelector("#resetbtn");
 
 playingToEle.textContent=winningScore;
 
+turnPlayer==="p2"
+? p1btnEle.setAttribute('disabled', 'disabled')
+: p2btnEle.setAttribute('disabled', 'disabled')
+
 //User input and update playing score
 inputEle.addEventListener("keypress", (e)=>{
     if(e.key === "Enter"){
         //e.preventDefault();
         //console.log(inputEle.value);
-        if(inputEle.value < 1){
+        let inputScore = inputEle.value;
+        if(inputScore < 1){
             alert("Please enter more than 0");
         }
-        winningScore=inputEle.value;
-        playingToEle.textContent=inputEle.value;
+        winningScore=+inputScore;
+        playingToEle.textContent=inputScore;
         //playingScore=playingToEle.textContent;
         inputEle.value="";
     }    
 });
 
 p1btnEle.addEventListener("click", (e)=>{
-    if(player1Score<winningScore){
-        player1Score++
+    if(turnPlayer==="p1"  && !gameOver && player1Score<winningScore){
+        player1Score++;
+        p1sEle.textContent=player1Score;
+        turnPlayer="p2";   
+        p1btnEle.setAttribute('disabled', 'disabled');
+        p2btnEle.removeAttribute('disabled');
     }
-    p1sEle.textContent=player1Score    
+    if(player1Score===winningScore){
+        gameOver=true;
+        p1btnEle.setAttribute('disabled', 'disabled');
+        p2btnEle.setAttribute('disabled', 'disabled');
+        alert("Player-1 is Winner!!!");
+    }
 });
 
 p2btnEle.addEventListener("click", (e)=>{
-    if(player2Score<winningScore){
-        player2Score++
+    if(turnPlayer==="p2" && !gameOver && player2Score<winningScore){
+        player2Score++;
+        p2sEle.textContent=player2Score; 
+        turnPlayer="p1";  
+        p2btnEle.setAttribute('disabled', 'disabled');
+        p1btnEle.removeAttribute('disabled');
     }
-    p2sEle.textContent=player2Score    
+    if(player2Score===winningScore){
+        gameOver=true;
+        p1btnEle.setAttribute('disabled', 'disabled');
+        p2btnEle.setAttribute('disabled', 'disabled');
+        alert("Player-2 is Winner!!!");
+    }
 });
 
+resetbtnEle.addEventListener("click", (e)=>{
+    player1Score=0;
+    player2Score=0;
+    winningScore=5;
+    gameOver=false;
+    turnPlayer="p1";
+    playingToEle.textContent=winningScore;
+    p1sEle.textContent=player1Score;
+    p2sEle.textContent=player2Score;
+    p1btnEle.removeAttribute('disabled');
+    p2btnEle.removeAttribute('disabled');
+})
